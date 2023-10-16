515acb0c291ca20519b83b21869faeee8eb21237
Alun Price <alunprice089@gmail.com>
2023-10-16 16:19:55 +0100

    Add a docker-compose.yml file and a redis image

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
 from typing import Union
 
 from fastapi import FastAPI
+import redis
 
 app = FastAPI()
 
+r = redis.Redis(host="redis", port=6379)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World134"}
+
+@app.get("/hits")
+def read_root():
+    r.incr("hits")
+    return {"Number of hits": r.get("hits")}
 
 
 @app.get("/items/{item_id}")
